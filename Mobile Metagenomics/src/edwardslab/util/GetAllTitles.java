5056bd2aa9d15594604f993a753df6878c7ab1d3
Keshisaru <Keshisaru@6ec63802-4fc5-11de-ac45-ff9ca5716a9b>
2010-03-11 22:56:05 +0000

    Finished UI for get all titles (unless we add level). Added dummy classes to fill in later for both options.

---
+++ Mobile Metagenomics/src/edwardslab/util/GetAllTitles.java
@@ -0,0 +1,57 @@
+package edwardslab.util;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class GetAllTitles extends Activity{
+	String fileName;
+	EditText chooseFileName;
+	String mode;
+	
+	public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filename);
+        chooseFileName = (EditText) findViewById(R.id.ChooseFilename);
+        final Button confirmButton = (Button) findViewById(R.id.Confirm);
+        fileName = "";
+        Bundle extras = getIntent().getExtras();
+        mode = extras.getString(MobileMetagenomics.CHOOSE_FILE_NAME_MODE);
+        
+        chooseFileName.setOnKeyListener(new OnKeyListener() {
+    	    public boolean onKey(View v, int keyCode, KeyEvent event) {
+    	        if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+    	          // Perform action on key press
+    	        	Bundle bundle = new Bundle();            
+    				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME, chooseFileName.getText().toString());
+    				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME_MODE, mode);
+    	            Intent mIntent = new Intent();
+    	            mIntent.putExtras(bundle);
+    	            setResult(RESULT_OK, mIntent);
+    	            finish();
+    	          return true;
+    	        }
+    	        return false;
+    	    }
+    	});
+        
+        confirmButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Bundle bundle = new Bundle();            
+				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME, chooseFileName.getText().toString());
+				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME_MODE, mode);
+	            Intent mIntent = new Intent();
+	            mIntent.putExtras(bundle);
+	            setResult(RESULT_OK, mIntent);
+	            finish();
+			}
+    	});
+	}
+}
