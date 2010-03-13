66d6a7921597c755bec19ec428ee030819a30089
Keshisaru <Keshisaru@6ec63802-4fc5-11de-ac45-ff9ca5716a9b>
2010-03-13 00:19:15 +0000

    Started work on GetJsonOrTitle. Most of the code was copy/paste from LoadWebChooser.

--- Mobile Metagenomics/src/edwardslab/util/GetJsonOrTitle.java
+++ Mobile Metagenomics/src/edwardslab/util/GetJsonOrTitle.java
@@ -1,56 +1,89 @@
 package edwardslab.util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class GetJsonOrTitle extends Activity{
-	String fileName;
-	EditText chooseFileName;
-	String mode;
+	String numberEntered;
+	Integer sampleEntered;
+	//String titleEntered;
+	EditText phoneNumber;
+	EditText sampleNumber;
+	//EditText title;
+	
 	
 	public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_json_or_title);
-        chooseFileName = (EditText) findViewById(R.id.ChooseFilename);
-        final Button confirmButton = (Button) findViewById(R.id.Confirm);
-        fileName = "";
-        Bundle extras = getIntent().getExtras();
-        mode = extras.getString(MobileMetagenomics.CHOOSE_FILE_NAME_MODE);
+        phoneNumber = (EditText) findViewById(R.id.PhoneNumber);
+        sampleNumber = (EditText) findViewById(R.id.SampleNumber);
+       // title = (EditText) findViewById(R.id.Title);
+        final Button myNumberButton = (Button) findViewById(R.id.UseMyNumber);
+        final Button confirmButton = (Button) findViewById(R.id.LoadWeb);
+        final Button getTitle = (Button) findViewById(R.id.GetTitle);
+        numberEntered = "";
+        sampleEntered = -1;
+      //  titleEntered = "";
         
-        chooseFileName.setOnKeyListener(new OnKeyListener() {
-    	    public boolean onKey(View v, int keyCode, KeyEvent event) {
-    	        if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-    	          // Perform action on key press
-    	        	Bundle bundle = new Bundle();            
-    				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME, chooseFileName.getText().toString());
-    				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME_MODE, mode);
-    	            Intent mIntent = new Intent();
-    	            mIntent.putExtras(bundle);
-    	            setResult(RESULT_OK, mIntent);
-    	            finish();
-    	          return true;
-    	        }
-    	        return false;
-    	    }
-    	});
         
         confirmButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Bundle bundle = new Bundle();            
-				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME, chooseFileName.getText().toString());
-				bundle.putString(MobileMetagenomics.CHOOSE_FILE_NAME_MODE, mode);
-	            Intent mIntent = new Intent();
-	            mIntent.putExtras(bundle);
-	            setResult(RESULT_OK, mIntent);
-	            finish();
+				try{
+					String tmpPhoneNumber = phoneNumber.getText().toString();
+					Integer tmpSampleNumber = Integer.parseInt((String)sampleNumber.getText().toString());
+					System.out.println("tmp Phone Number is: " + tmpPhoneNumber.toString());
+					System.out.println("sample number is: " + tmpSampleNumber.toString());
+					if(!tmpPhoneNumber.equals("")){
+						numberEntered = tmpPhoneNumber;
+					}
+					if(tmpSampleNumber != 0){
+						sampleEntered = tmpSampleNumber;
+					}
+					
+					/*if(!title.getText().toString().equals("")){
+						titleEntered = title.getText().toString();
+					}*/
+					
+					Bundle bundle = new Bundle();            
+					bundle.putString(MobileMetagenomics.LOAD_FILE_PHONE_NUMBER, numberEntered);
+					bundle.putInt(MobileMetagenomics.LOAD_FILE_SAMPLE_NUMBER, sampleEntered);
+					/*if(!titleEntered.equals("no title")){
+						bundle.putString(MobileMetagenomics.LOAD_FILE_SAMPLE_TITLE, titleEntered);
+					}*/
+		            Intent mIntent = new Intent();
+		            mIntent.putExtras(bundle);
+		            setResult(RESULT_OK, mIntent);
+		            finish();
+				}
+				catch(Exception E){
+					
+				}
+			}
+    	});
+        
+        myNumberButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TelephonyManager mTelephonyMgr;  
+				mTelephonyMgr = (TelephonyManager)  
+				getSystemService(Context.TELEPHONY_SERVICE);
+				String tmpLineNumber = mTelephonyMgr.getLine1Number();
+				phoneNumber.setText(tmpLineNumber);
+			}
+    	});
+        
+        getTitle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
 			}
     	});
 	}
